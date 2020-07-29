c1d031c265b97f2c65bf6f8dc68b300b1ae2f1d7
bekah <elisaalvarez211@gmail.com>
2020-07-29 10:07:07 -0700

    Finish Adding files

---
+++ fitness/src/reducer/deleteClassReducer.js
@@ -0,0 +1,36 @@
+import {
+    DELETE_CLASS_START,
+    DELETE_CLASS_SUCCESS,
+    DELETE_CLASS_FAIL 
+} from '../actions/deleteClass'
+
+const initalState = {
+    class: {},
+    error: '',
+    isFetching: false
+}
+
+export const deleteClassReducer = ( state=initalState, action) => {
+    switch(action.type){
+        case DELETE_CLASS_START:
+            return {
+                ...state,
+                isFetching: true,
+                error: ''
+            };
+        case DELETE_CLASS_SUCCESS:
+            return {
+                ...state,
+                class: action.payload,
+                isFetching: false,
+                error: ''
+            };
+        case DELETE_CLASS_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            };
+        default:
+            return state
+    }
+}
